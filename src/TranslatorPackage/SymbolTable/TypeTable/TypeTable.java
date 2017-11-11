a07f38bc14d872241e4a090403c0f4eb16c34f73
Scarecrow <limo97@163.com>
2017-11-11 11:46:05 +0800

    分支合并

--- src/TranslatorPackage/SymbolTable/TypeTable/TypeTable.java
+++ src/TranslatorPackage/SymbolTable/TypeTable/TypeTable.java
@@ -1,6 +1,6 @@
 package TranslatorPackage.SymbolTable.TypeTable;
 
-import TranslatorPackage.SymbolTable.SemanticExcption;
+import TranslatorPackage.SymbolTable.SemanticException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,7 +20,7 @@ public class TypeTable {
         //basic 是最最基本的类型了 在生成目标代码的时候就直接分配内存大小了 不再递归沿着type往下查
         tableRowMap.put("int", basicType);
 
-        basicType = new TypeTableRow("float");
+        basicType = new TypeTableRow("double");
         basicType.addField("value", 1, "basic");
         tableRowMap.put("float", basicType);
 
@@ -30,10 +30,10 @@ public class TypeTable {
     }
 
     //从类型名获取类型info
-    public TypeTableRow getTypeInfo(String type_name) throws SemanticExcption {
+    public TypeTableRow getTypeInfo(String type_name) throws SemanticException {
         TypeTableRow res = tableRowMap.get(type_name);
         if (res == null)
-            throw new SemanticExcption("type :" + type_name + "has not declare");
+            throw new SemanticException("type :" + type_name + "has not declare");
         return tableRowMap.get(type_name);
     }
 
@@ -43,9 +43,9 @@ public class TypeTable {
      * @param type_name 类型
      *  是否添加成功 重复添加类型会抛exception
      */
-    public String declareType(String type_name) throws SemanticExcption {
+    public String declareType(String type_name) throws SemanticException {
         if (tableRowMap.get(type_name) != null)
-            throw new SemanticExcption("repeating declare type, type name : " + type_name + " has occurred");
+            throw new SemanticException("repeating declare type, type name : " + type_name + " has occurred");
         if (!type_name.equals("basic"))
             tableRowMap.put(type_name, new TypeTableRow(type_name));
         return type_name;
@@ -59,15 +59,15 @@ public class TypeTable {
      * @param field_type 要添加进去域类型
      * @param field_name 域的名字 也不能重
      */
-    public String addFieldOnType(String type_name, String field_type, String field_name) throws SemanticExcption {
+    public String addFieldOnType(String type_name, String field_type, String field_name) throws SemanticException {
         TypeTableRow selectedType = tableRowMap.get(type_name);
         if(selectedType.getField(field_name) != null)
-            throw new SemanticExcption("field name : " + field_name + " has already exist");
+            throw new SemanticException("field name : " + field_name + " has already exist");
 
         TypeTableRow addFieldType = tableRowMap.get(field_type);
 
         if(addFieldType == null)
-            throw new SemanticExcption("no such type : " + field_type + " which add as a field");
+            throw new SemanticException("no such type : " + field_type + " which add as a field");
 
         selectedType.addField(field_name, addFieldType.getLength(), field_type);
         return selectedType.getName();
@@ -77,10 +77,10 @@ public class TypeTable {
      * 新建数组类型的实时会调用这个方法
      * @param arr_elem_type_name 添加的数组内元素类型
      * @param array_len          数组长度
-     * @throws SemanticExcption 数组元素类型未声明直接抛出异常
+     * @throws SemanticException 数组元素类型未声明直接抛出异常
      */
 
-    public String declareArrayType(String arr_elem_type_name, int array_len) throws SemanticExcption {
+    public String declareArrayType(String arr_elem_type_name, int array_len) throws SemanticException {
         String array_type_name = "array_" + arr_elem_type_name + "_" + array_len;
         //数组类型的类型名有这样的格式 ： array_元素类型_长度
         TypeTableRow selectedType = tableRowMap.get(array_type_name);
@@ -91,7 +91,7 @@ public class TypeTable {
         TypeTableRow arr_elem_type = tableRowMap.get(arr_elem_type_name);
         //检查数组类型是否存在
         if (arr_elem_type == null)
-            throw new SemanticExcption("type : " + arr_elem_type_name + " has not declared before .");
+            throw new SemanticException("type : " + arr_elem_type_name + " has not declared before .");
 
         //添加数组类型表项
         declareType(array_type_name);
