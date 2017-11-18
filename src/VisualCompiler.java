a8394a3b8506a112bb48377eed84aebb1491a387
Aaron Zhang <troublor@live.com>
2017-11-18 11:48:19 +0800

    图形界面revert

---
+++ src/VisualCompiler.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class VisualCompiler extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Visual Compiler");
+        Pane myPane = (Pane) FXMLLoader.load(getClass().getResource
+                ("VisualCompiler.fxml"));
+        Scene myScene = new Scene(myPane);
+        primaryStage.setScene(myScene);
+        primaryStage.show();
+    }
+}
